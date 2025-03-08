d0d0845f6680d578bb5781794f90a813798b19a4
robertredbox <robert@redboxmobile.com>
2025-03-08 00:15:13 +0000

    Update root layout to include required fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,17 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <style jsx global>{`
+            body {
+              font-family: 'Roboto', sans-serif;
+              font-weight: 400;
+            }
+            
+            h1, h2, h3, h4, h5, h6 {
+              font-family: 'Roboto Slab', serif;
+              font-weight: 500;
+            }
+          `}</style>
           {children}
         </ThemeProvider>
       </body>
